b719fe31743b0704992b3cbc4bb6579d67cedc13
Miguel_Cassar <mcassar@onpointserv.com>
2025-04-04 17:08:44 +0200

    created db, repository, DbContext and poll

---
+++ DataAccess/Repositories/PollRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.DataContext;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class PollRepository
+    {
+        private readonly PollDbContext _context;
+
+        public PollRepository(PollDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreatePoll(string title, string option1Text, string option2Text, string option3Text)
+        {
+            var poll = new Poll
+            {
+                Title = title,
+                Option1Text = option1Text,
+                Option2Text = option2Text,
+                Option3Text = option3Text,
+                Option1VotesCount = 0,
+                Option2VotesCount = 0,
+                Option3VotesCount = 0,
+                DateCreated = DateTime.UtcNow
+            };
+
+            _context.Polls.Add(poll);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
