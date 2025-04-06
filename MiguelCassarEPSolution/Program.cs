1f747a62b63f220f84d1789a804a0ad70b7517a6
Miguel_Cassar <mcassar@onpointserv.com>
2025-04-06 15:53:12 +0200

    fixes

--- MiguelCassarEPSolution/Program.cs
+++ MiguelCassarEPSolution/Program.cs
@@ -3,6 +3,13 @@ using Microsoft.EntityFrameworkCore;
 using DataAccess.DataContext;
 using DataAccess.Repositories;
 using Presentation.ActionFilter;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.UI;
+using Domain.Models;
+using Presentation.Controllers;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
 
 namespace MiguelCassarEPSolution
 {
@@ -12,57 +19,54 @@ namespace MiguelCassarEPSolution
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            //builder.Services.AddDbContext<PollDbContext>(options =>
-            //    options.UseSqlServer(connectionString));
-
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
             builder.Services.AddDbContext<PollDbContext>(options =>
                 options.UseNpgsql(connectionString));
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<PollDbContext>();
-            builder.Services.AddControllersWithViews();
 
             builder.Services.AddScoped<IPollRepository, PollRepository>();
-            //builder.Services.AddScoped<IPollRepository, PollFileRepository>();
-
+            builder.Services.AddScoped<IPollRepository, PollFileRepository>();
             builder.Services.AddScoped<VoteLogRepository>();
-
             builder.Services.AddScoped<OneVotePerUserFilter>();
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+            builder.Logging.AddConsole();
 
             builder.WebHost.ConfigureKestrel(serverOptions =>
             {
-                serverOptions.ListenAnyIP(5000);
+                serverOptions.ListenAnyIP(5000); // Always run on 5000
             });
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (!app.Environment.IsDevelopment())
             {
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
+                // Trust headers from Nginx (so redirect loop doesn't happen)
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedFor,
+                    KnownProxies = { IPAddress.Parse("locahost") }
+                });
+
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection(); // Enable HTTPS in prod only
             }
 
-            //app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
+            app.MapRazorPages();
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-            app.MapRazorPages();
 
             app.Run();
         }
